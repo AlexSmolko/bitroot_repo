f0f609bf4882cc19fabcba81d7d6c0cef41a9498
Aresandro <34611935+Aresnadro@users.noreply.github.com>
2020-05-07 21:57:43 +0300

    Update Task1.py
    Added comments about an emerging message as a result of changing the type of error

--- Lesson_8 (Exceptions)/Task1.py
+++ Lesson_8 (Exceptions)/Task1.py
@@ -1,5 +1,5 @@
 def oops():
-    raise KeyError
+    raise IndexError
 
 def trapper():
     try:
@@ -9,3 +9,10 @@ def trapper():
         oops()
 
 trapper()
+
+"""So, when we replace the IndexError with KeyError, but not change the condition and it will be the same 
+that will lead to raising of the IndexError, it will print the messages: 'IndexError: list index out of range'
+and the next:
+'During handling of the above exception, another exception occurred:' 
+And the next message will be: 'raise KeyError' and on the next line: 'KeyError' """
+
